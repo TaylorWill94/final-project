27baedf594387d7cd63f577b3c1fbb8398d23709
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2025-11-04 11:58:42 -0500

    Adds informative paragraphs

--- src/App.js
+++ src/App.js
@@ -13,6 +13,26 @@ function App() {
       <div>
         <p>Taylor Williams</p>
       </div>
+      <div>
+        <h1>Venice</h1>
+        <p>
+          Venice is a beautiful city with amazing charm and wonder. Surronded by
+          the beautiful waters, Venice is a lover's dream. But throughout its
+          beauty, Venice is facing a dire problem. Venice is sinking. You may
+          think that isn't an issue being that Venice is surronded by water, but
+          it is more of an issue than you think.
+        </p>
+      </div>
+
+      <div>
+        <p>
+          This issue is important for making a city resilient in the future
+          because as we face global warming, sinking isn't just an issue in
+          Venice. Addressing hoow to fix this issue is important to the
+          sustanability of many cities throughout the world. As well as making
+          sure that there are proper protection mesasures in place.
+        </p>
+      </div>
     </div>
   );
 }
