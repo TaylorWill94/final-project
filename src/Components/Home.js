ea1eebed7ad672afa1bc9f827c03be95a583a6ab
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2025-11-04 20:24:34 -0500

    working on the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,10 @@ function Home() {
   return (
     <div className="home">
       <h1>21st Century Cities</h1>
+      <p>
+        This site is dedicated to explore why Venice is sinking and offer
+        solutions.
+      </p>
     </div>
   );
 }
