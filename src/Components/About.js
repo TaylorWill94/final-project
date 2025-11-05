f0fa491bc920c57e0198546ca987a32fabac5b79
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2025-11-04 20:36:02 -0500

    Changes the font color

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,6 +4,11 @@ function About() {
   return (
     <div className="about">
       <h1 className="name">Taylor Williams</h1>
+      <p className="bio">
+        Taylor Williams is a New York native. Born and raised in Brooklyn, NY,
+        Taylor later on relocated to Queens, NY. Taylor has always had a passion
+        for technology, writing, and fashion.
+      </p>
     </div>
   );
 }
